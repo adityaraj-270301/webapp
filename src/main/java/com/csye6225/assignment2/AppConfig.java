d5772e47d511a50778bae7ae5ec30d42887df9ca
adityaraj-2703 <113265414+adityaraj-2703@users.noreply.github.com>
2023-10-04 18:57:45 -0400

    Update AppConfig.java

--- src/main/java/com/csye6225/assignment2/AppConfig.java
+++ src/main/java/com/csye6225/assignment2/AppConfig.java
@@ -32,7 +32,7 @@ public class AppConfig {
 
     @PostConstruct
     public void loadUsersFromCSV() throws IOException {
-        List<UserAccount> userAccounts = userAccountService.loadUsersFromCSV("src/main/resources/users.csv");
+        List<UserAccount> userAccounts = userAccountService.loadUsersFromCSV("opt/users.csv");
         userAccountService.saveAll(userAccounts);
     }
 
